d460cd77869bcbbdfcf9d5172d4e41d965a751c3
anangga.dipa <bugs1945@gmail.com>
2020-12-31 18:32:46 +0800

    remove add group from group list

--- lib/widget/group_list.dart
+++ lib/widget/group_list.dart
@@ -1,9 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_colorpicker/flutter_colorpicker.dart';
-import 'package:geocoder/geocoder.dart';
 import 'package:khotmil/constant/helper.dart';
 import 'package:khotmil/constant/text.dart';
-import 'package:khotmil/fetch/create_group.dart';
 import 'package:khotmil/fetch/my_group_list.dart';
 import 'package:khotmil/widget/add_group.dart';
 import 'package:khotmil/widget/group_detail.dart';
@@ -23,81 +20,14 @@ class GroupList extends StatefulWidget {
 
 class _GroupListState extends State<GroupList> {
   final GlobalKey<ScaffoldState> scaffoldKey = GlobalKey<ScaffoldState>();
-  final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
 
   Future _futureGetGroupList;
   String _screenState = StateGroupList;
   String _messageText = '';
 
-  Color _currentColor = Color(0xfff6d55c);
-  bool _showColorPicker = false;
-
-  bool _searchLatlong = false;
-  bool _hasAddressData = false;
-  String _addressSeacrhError = '';
-  Address _addressSuggestion;
-
-  TextEditingController _nameFormController = TextEditingController();
-  TextEditingController _addressFormController = TextEditingController();
-  TextEditingController _colorFormController = TextEditingController();
-  TextEditingController _endDateFormController = TextEditingController();
-  TextEditingController _uidsFormController = TextEditingController();
-  FocusNode _focusAddressNode = FocusNode();
-
-  Future<void> _renderSelectDate(BuildContext context) async {
-    DateTime date = DateTime.now();
-    final DateTime picked = await showDatePicker(
-      context: context,
-      initialDate: DateTime.now(),
-      firstDate: date,
-      lastDate: DateTime(date.year, date.month + 3),
-      helpText: FormCreateGroupEndDate,
-    );
-    if (picked != null) _endDateFormController.text = (picked.toString()).split(' ')[0];
-  }
-
   void _apiGroupList() {
     setState(() {
       _futureGetGroupList = fetchMyGroupList(widget.loginKey);
-    });
-  }
-
-  void _apiCreateGroup() async {
-    widget.toggleLoading();
-    setState(() {
-      _messageText = '';
-    });
-
-    await fetchCreateGroup(
-      widget.loginKey,
-      _nameFormController.text,
-      _addressFormController.text,
-      _addressSuggestion.coordinates.latitude.toString() + ',' + _addressSuggestion.coordinates.longitude.toString(),
-      _currentColor.value.toRadixString(16).substring(2).toUpperCase(),
-      _endDateFormController.text,
-      _uidsFormController.text.split(','),
-    ).then((data) {
-      widget.toggleLoading();
-      if (data[DataStatus] == StatusSuccess) {
-        setState(() {
-          _futureGetGroupList = fetchMyGroupList(widget.loginKey);
-          _screenState = StateGroupList;
-          _nameFormController.text = '';
-          _addressFormController.text = '';
-          _colorFormController.text = '';
-          _endDateFormController.text = '';
-          _uidsFormController.text = '';
-        });
-      } else if (data[DataStatus] == StatusError) {
-        setState(() {
-          _messageText = data[DataMessage];
-        });
-      }
-    }).catchError((onError) {
-      widget.toggleLoading();
-      setState(() {
-        _messageText = onError.toString();
-      });
     });
   }
 
@@ -112,7 +42,7 @@ class _GroupListState extends State<GroupList> {
     setState(() {
       switch (action) {
         case 1:
-          _screenState = StateJoinGroup;
+          _screenState = '';
           break;
         case 2:
           _screenState = StateGroupList;
@@ -123,41 +53,6 @@ class _GroupListState extends State<GroupList> {
           break;
       }
     });
-  }
-
-  void _changeColor(Color color) {
-    setState(() {
-      _currentColor = color;
-      _colorFormController.text = '#' + _currentColor.value.toRadixString(16).substring(2).toUpperCase();
-      _showColorPicker = false;
-    });
-  }
-
-  void _getLatLong() async {
-    setState(() {
-      _hasAddressData = false;
-      _addressSeacrhError = '';
-      _searchLatlong = true;
-    });
-    await Geocoder.local.findAddressesFromQuery(_addressFormController.text).then((data) {
-      setState(() {
-        _addressSuggestion = data.first;
-        _hasAddressData = true;
-        _searchLatlong = false;
-      });
-    }).catchError((onError) {
-      setState(() {
-        _addressSeacrhError = AddressSuggestionErrorText;
-        _searchLatlong = false;
-      });
-    });
-  }
-
-  String _getTimeStamp() {
-    String ts = _endDateFormController.text == ''
-        ? DateTime.now().millisecondsSinceEpoch.toString()
-        : DateTime.parse(_endDateFormController.text + ' 00:00:00.000').millisecondsSinceEpoch.toString();
-    return ts.substring(0, ts.length - 3);
   }
 
   Widget _loopGroups(groups, context) {
@@ -220,154 +115,6 @@ class _GroupListState extends State<GroupList> {
     );
   }
 
-  Widget _createGroupForm() {
-    return Form(
-      key: _formKey,
-      child: Container(
-          padding: mainPadding,
-          child: Column(
-            children: [
-              SizedBox(height: 16.0),
-              Text(CreateGroup, style: bold),
-              SizedBox(height: 16.0),
-              if (_nameFormController.text != '')
-                GroupItem(
-                  groupName: _nameFormController.text,
-                  progress: '95',
-                  round: '1',
-                  deadline: _getTimeStamp(),
-                  yourJuz: '25',
-                  yourProgress: '50',
-                  groupColor: _currentColor.value.toRadixString(16).substring(2).toUpperCase(),
-                ),
-              SizedBox(height: 16.0),
-              TextFormField(
-                controller: _nameFormController,
-                keyboardType: TextInputType.text,
-                decoration: const InputDecoration(
-                  contentPadding: EdgeInsets.symmetric(horizontal: 8.0),
-                  hintText: FormCreateGroupName,
-                ),
-                validator: (value) {
-                  if (value.isEmpty) {
-                    return FormCreateGroupNameError;
-                  }
-                  return null;
-                },
-              ),
-              TextFormField(
-                controller: _addressFormController,
-                keyboardType: TextInputType.text,
-                decoration: const InputDecoration(
-                  contentPadding: EdgeInsets.symmetric(horizontal: 8.0),
-                  hintText: FormCreateGroupAddress,
-                ),
-                validator: (value) {
-                  if (value.isEmpty) {
-                    return FormCreateGroupAddressError;
-                  }
-                  return null;
-                },
-                focusNode: _focusAddressNode,
-              ),
-              if (_searchLatlong)
-                Container(
-                    padding: verticalPadding,
-                    child: Column(children: [
-                      Text(AddressValidateTitle, style: bold),
-                      SizedBox(height: 16.0),
-                      Center(child: CircularProgressIndicator()),
-                    ])),
-              if (_addressSeacrhError != '')
-                Container(
-                    width: MediaQuery.of(context).size.width,
-                    padding: verticalPadding,
-                    child: Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
-                      Text(_addressSeacrhError, style: bold),
-                      SizedBox(height: 16.0),
-                      Text(ClosedAddressFoundDesc),
-                    ])),
-              if (_hasAddressData)
-                Container(
-                    width: MediaQuery.of(context).size.width,
-                    padding: verticalPadding,
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        Text(ClosedAddressFoundTitle, style: bold),
-                        SizedBox(height: 16.0),
-                        Text(_addressSuggestion.addressLine),
-                        SizedBox(height: 8.0),
-                        Text(FormCreateGroupLatlong + _addressSuggestion.coordinates.latitude.toString() + ',' + _addressSuggestion.coordinates.longitude.toString()),
-                        SizedBox(height: 8.0),
-                        RaisedButton(
-                          onPressed: () {
-                            setState(() {
-                              _hasAddressData = false;
-                            });
-                          },
-                          child: Text('Ok'),
-                        ),
-                        SizedBox(height: 16.0),
-                        Text(ClosedAddressFoundDesc),
-                      ],
-                    )),
-              Container(
-                decoration: BoxDecoration(color: _currentColor),
-                child: TextFormField(
-                  controller: _colorFormController,
-                  keyboardType: TextInputType.text,
-                  readOnly: true,
-                  onTap: () {
-                    setState(() {
-                      _showColorPicker = !_showColorPicker;
-                    });
-                  },
-                  style: TextStyle(color: _currentColor.computeLuminance() > 0.5 ? Colors.black : Colors.white),
-                  decoration: const InputDecoration(
-                    contentPadding: EdgeInsets.symmetric(horizontal: 8.0),
-                    hintText: FormCreateGroupColor,
-                  ),
-                ),
-              ),
-              if (_showColorPicker)
-                Container(
-                  height: MediaQuery.of(context).size.width * 0.8,
-                  child: MaterialPicker(
-                    pickerColor: _currentColor,
-                    onColorChanged: _changeColor,
-                    enableLabel: true,
-                  ),
-                ),
-              TextFormField(
-                controller: _endDateFormController,
-                keyboardType: TextInputType.text,
-                readOnly: true,
-                onTap: () => _renderSelectDate(context),
-                decoration: const InputDecoration(
-                  contentPadding: EdgeInsets.symmetric(horizontal: 8.0),
-                  hintText: FormCreateGroupEndDate,
-                ),
-                validator: (value) {
-                  if (value.isEmpty) {
-                    return FormCreateGroupEndDateError;
-                  }
-                  return null;
-                },
-              ),
-              TextFormField(
-                controller: _uidsFormController,
-                keyboardType: TextInputType.text,
-                decoration: const InputDecoration(
-                  contentPadding: EdgeInsets.symmetric(horizontal: 8.0),
-                  hintText: FormCreateGroupUids,
-                ),
-              ),
-            ],
-          )),
-    );
-  }
-
   Widget _joinGroupForm() {
     return Container(
       padding: mainPadding,
@@ -379,26 +126,6 @@ class _GroupListState extends State<GroupList> {
   void initState() {
     super.initState();
     _apiGroupList();
-    // _endDateFormController.text = (DateTime.now().toString()).split(' ')[0];
-    _colorFormController.text = '#' + _currentColor.value.toRadixString(16).substring(2).toUpperCase();
-
-    // handle search address coordinate
-    _focusAddressNode.addListener(() {
-      if (_addressFormController.text != '' && !_focusAddressNode.hasFocus) {
-        _getLatLong();
-      }
-    });
-  }
-
-  @override
-  void dispose() {
-    _nameFormController.dispose();
-    _addressFormController.dispose();
-    _colorFormController.dispose();
-    _endDateFormController.dispose();
-    _uidsFormController.dispose();
-    _focusAddressNode.dispose();
-    super.dispose();
   }
 
   @override
@@ -432,8 +159,8 @@ class _GroupListState extends State<GroupList> {
                 child: ConstrainedBox(
                     constraints: BoxConstraints(minWidth: MediaQuery.of(context).size.width),
                     child: Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
+                      if (_screenState == '') Text(''),
                       if (_screenState == StateGroupList) _myGroupList(context),
-                      if (_screenState == StateCreateGroup) _createGroupForm(),
                       if (_screenState == StateJoinGroup) _joinGroupForm(),
                     ]))),
           ),
@@ -444,19 +171,8 @@ class _GroupListState extends State<GroupList> {
             child: Row(
               mainAxisAlignment: MainAxisAlignment.spaceEvenly,
               children: [
-                if (_screenState == StateCreateGroup)
-                  RaisedButton(
-                      onPressed: () {
-                        if (_formKey.currentState.validate()) {
-                          _apiCreateGroup();
-                        }
-                      },
-                      child: Text(SubmitText)),
-                if (_screenState == StateCreateGroup)
-                  RaisedButton(onPressed: () => _toggleScreenState(StateCreateGroup), child: Text(FormCreateGroupPreview), color: Color(0xfff6d55c)),
-                if (_screenState == StateCreateGroup || _screenState == StateJoinGroup)
-                  RaisedButton(onPressed: () => _toggleScreenState(StateGroupList), child: Text(CancelText), color: Colors.blueGrey),
-                if (_screenState != StateCreateGroup && _screenState != StateJoinGroup)
+                if (_screenState == StateJoinGroup) RaisedButton(onPressed: () => _toggleScreenState(StateGroupList), child: Text(CancelText), color: Colors.blueGrey),
+                if (_screenState != StateJoinGroup)
                   RaisedButton(
                       onPressed: () {
                         Navigator.push(context, MaterialPageRoute(builder: (context) {
@@ -465,7 +181,7 @@ class _GroupListState extends State<GroupList> {
                       },
                       child: Text(CreateGroup),
                       color: Colors.redAccent),
-                if (_screenState != StateCreateGroup && _screenState != StateJoinGroup) RaisedButton(onPressed: () => _toggleScreenState(StateJoinGroup), child: Text(JoinGroup)),
+                if (_screenState != StateJoinGroup) RaisedButton(onPressed: () => _toggleScreenState(StateJoinGroup), child: Text(JoinGroup)),
               ],
             ),
           )
